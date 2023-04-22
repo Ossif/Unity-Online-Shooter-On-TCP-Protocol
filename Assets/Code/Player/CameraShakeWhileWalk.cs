214a44c2afb4233da3d526b86f0cefdbefd3f810
Ossif <102289530+Ossif@users.noreply.github.com>
2023-04-22 23:34:28 +0700

    movement_fix+soilder_model

--- Assets/Code/Player/CameraShakeWhileWalk.cs
+++ Assets/Code/Player/CameraShakeWhileWalk.cs
@@ -11,37 +11,22 @@ public class CameraShakeWhileWalk : MonoBehaviour
     public float speed;
 
     private Vector3 startPos;
-    private bool _isGrounded;
     private bool _stopCamera;
+
+    private CharacterController cc;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = cam.transform.localPosition;
-    }
-
-
-
-    void OnCollisionEnter(Collision collision)
-    {
-        _isGrounded = true;
-    }
-
-    void OnCollisionStay(Collision coll)
-    {     
-        _isGrounded = true;
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        _isGrounded = false;
+        cc = gameObject.GetComponent<CharacterController>();
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(_isGrounded == true && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))
+        if(cc.isGrounded == true && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))
         {
             Vector3 translation = new Vector3( A * Mathf.Cos(time + Mathf.PI/2), B * Mathf.Sin(Mathf.PI/2 - 2*time), 0 );
             cam.transform.localPosition = startPos + translation;
--- Assets/Code/Player/Movement.cs
+++ Assets/Code/Player/Movement.cs
@@ -5,125 +5,43 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-     public float Speed = 0.3f;
+    private CharacterController cc;
+    private Rigidbody rb;
+
+    public float Speed = 0.3f;
     public float JumpForce = 1f;
-
-    private Rigidbody _rb;
-    private CapsuleCollider _collider; 
-
-    public bool _isGrounded = true;
-
+    public float Gravity = 9.8f;
+    
+    private Vector3 actualVel = Vector3.zero;
     //Анимация
     public GameObject girl;
 
-    /*public AnimatorController idle;
-    public AnimatorController forward;
-    public AnimatorController back;
-    public AnimatorController left;
-    public AnimatorController right;*/
-
-    void OnCollisionEnter(Collision collision)
-    {
-        _isGrounded = true;
-    }
-    
-    void OnCollisionStay(Collision coll)
-    {     
-        _isGrounded = true;
-    }
-
-     void OnCollisionExit(Collision collision)
-     {
-        _isGrounded = false;
-    }
-
     void Start()
     {
-        //animator = girl.GetComponent<Animator>();
-
-        _rb = GetComponent<Rigidbody>();
-        _collider = GetComponent<CapsuleCollider>();
+        cc = gameObject.GetComponent<CharacterController>();
     }
 
     void FixedUpdate()
     {
-        MoveLogic();
-        JumpLogic();
-    }
 
-    void MoveLogic(){
-        float currVelY = _rb.velocity.y;
-        Vector3 actualVel = new Vector3();
+        
         Vector3 horV = new Vector3();
         Vector3 verV = new Vector3();
 
-        //int newAnimId = 0;
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            //newAnimId = 4;
-            horV = transform.right * Speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            //newAnimId = 3;
-            horV = -transform.right * Speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            //newAnimId = 1;
-            verV = transform.forward * Speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            //newAnimId = 2;
-            verV = -transform.forward * Speed * Time.deltaTime;
-        }
+        horV = transform.right * Speed * Input.GetAxis("Horizontal");
+        verV = transform.forward * Speed * Input.GetAxis("Vertical");
         
-        
+        float movementDirectionY = actualVel.y;
         actualVel = verV + horV;
-
-        actualVel.y = currVelY;
-        _rb.velocity = actualVel;
-
-        /*if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D)){
-            //newAnimId = 0;
-            _rb.velocity = new Vector3(0, _rb.velocity.y, 0);
-        }*/
-
-        /*if(animId != newAnimId){
-            animId = newAnimId;
-            switch (animId)
-            {
-                case 0:
-                    animator.Play("Idle");
-                    break;
-                case 1:
-                    animator.Play("RForward");
-                    break;
-                case 2:
-                    animator.Play("RBack");
-                    break;
-                case 3:
-                    animator.Play("RLeft");
-                    break;
-                case 4:
-                    animator.Play("RRight");
-                    break;
-
-            }
-        }*/
-    }
-
-    private void JumpLogic()
-    {
-        if (_isGrounded && (Input.GetAxis("Jump") > 0))
+        
+        if (cc.isGrounded && (Input.GetAxis("Jump") > 0))
         {
-            //_rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-            Vector3 actualVector = Vector3.up * JumpForce;
-            actualVector.x = _rb.velocity.x;
-            actualVector.z = _rb.velocity.z;
-            _rb.velocity = actualVector;
+            actualVel.y = JumpForce;
         }
+        else {
+            actualVel.y = movementDirectionY;
+        }
+        actualVel.y -= Gravity * Time.deltaTime;
+        cc.Move(actualVel * Time.deltaTime);
     }
 }
